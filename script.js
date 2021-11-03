ff4fac033ac3ec8544f4d12bc4063a9b5eaa56ea
AnnaTskhadadze <anatskhadadze1@gmail.com>
2021-11-03 19:47:36 +0400

    js davaleba

---
+++ script.js
@@ -0,0 +1,27 @@
+var x = 100
+var y = 50
+var z= 40
+
+if ( x > y && x > z ) {
+    console.log(x)
+} else if ( y > x && y > z ) {
+    console.log(y)
+} else {
+    console.log(z)
+}
+
+
+
+var score = 55;
+
+if (score > 90 ) {
+    console.log(" A")
+}  else if ( score > 80 ) {
+    console.log(' B')
+} else if ( score > 70 ) {
+    connsole.log(' C')
+} else if ( score > 60 ) {
+    console.log(' D')
+} else if ( score <= 60 ) {
+    console.log(' F')
+}
